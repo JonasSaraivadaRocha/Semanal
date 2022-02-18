ce337d2e5611c6e75ce70b80466be28fbe5af309
Jonas Saraiva da Rocha <jonasrochaxd@hotmail.com>
2022-02-17 22:15:28 -0400

    incluindo regras

--- script.js
+++ script.js
@@ -20,9 +20,31 @@ function capturar() {
                     break;
                 }
             else {
-                document.getElementById('validou').innerHTML = "IP válido";
+                document.getElementById('validou').innerHTML = "Foi digitado uma estrutura correta de IP";
+                valida_classe(validaIP[0])
+
             }
           }
     }
 
 }
+
+function valida_classe(oc1) {
+    if (oc1 >=1 && oc1 <=126 ){
+        document.getElementById('classe').innerHTML = "Este IP é de classe A, disponível para internet";
+    }
+    else if (oc1 >=128 && oc1 <=191 ){
+        document.getElementById('classe').innerHTML = "Este IP é de classe B, disponível para internet";
+    }
+    else if (oc1 >=192 && oc1 <=223 ){
+        document.getElementById('classe').innerHTML = "Este IP é de classe C disponível para internet";
+    }
+    else if (oc1 >=224 && oc1 <=239 ){
+        document.getElementById('classe').innerHTML = "Este IP é de classe D e é para Multicast (manda mensagem para vários hosts da rede), não é disponível para internet";
+    }
+    else if (oc1 >=240 && oc1 <=255 ){
+        document.getElementById('classe').innerHTML = "Este IP é de classe E, classe reservada para testes de novas tecnologias";
+    }
+}
+
+
